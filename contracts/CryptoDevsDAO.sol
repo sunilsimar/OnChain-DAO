a94a8e04062e8c3f57c4a9f36736517b0214640c
Sunil <sunisimar2002d@gmail.com>
2023-10-05 11:34:57 +0530

    created contract for DAO

---
+++ contracts/CryptoDevsDAO.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+interface IFakeNFTMarketPlace {
+    function getPrice() external view returns (uint256);
+
+    function available(uint256 _tokenId) external view returns (bool);
+
+    function purchase(uint256 _tokenId) external payable;
+}
+
+interface ICryptoDevsNFT {
+    function balanceOf(address owner) external view returns (uint256);
+
+    function tokenOfOwnerByIndex(
+        address owner,
+        uint256 index
+    ) external view returns (uint256);
+}
+
+contract CryptoDevsDAO is Ownable {
+    struct Proposal {
+        uint256 nftTokenId;
+        uint256 deadline;
+        uint256 yayVotes;
+        uint256 nayVotes;
+        bool executed;
+        mapping(uint256 => bool) voters;
+    }
+
+    mapping(uint256 => Proposal) public proposals;
+
+    uint256 public numProposals;
+
+    IFakeNFTMarketPlace nftMarketPlace;
+    ICryptoDevsNFT cryptoDevsNFT;
+
+    constructor(address _nftMarketplace, address _cryptoDevsNFT) payable {
+        nftMarketPlace = IFakeNFTMarketPlace(_nftMarketplace);
+        cryptoDevsNFT = ICryptoDevsNFT(_cryptoDevsNFT);
+    }
+
+    modifier nftHolderOnly() {
+        require(cryptoDevsNFT.balanceOf(msg.sender) > 0, "NOT_A_DAO_MEMBER");
+        _;
+    }
+}
